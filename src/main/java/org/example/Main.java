3d9055fc362dc9e191611985a0c0f6e3fca28e71
twiwter <alexander.karadyaur@gmail.com>
2023-07-22 12:58:29 +0200

    modified: newFile.file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,5 @@
 package org.example;
 
-// Задать массив из N элементов и вывести их на экран
-// 1. Создать массив
-//    int[] createArray(int size)
-// 2. Заполнение массива
-//    2.1. Рандомными числами
-//    + void generateArray(int[] arr)
-//    int[] generateArray(int[] arr)
-// 3. Вывести на экран
-//    3.1. терминал
-//    void printArray(int[] arr)
-
-
 public class Main {
     public static void main(String[] args) {
 
