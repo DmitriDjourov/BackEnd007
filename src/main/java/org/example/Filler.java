ed17fa67ed58a77d4d8cc3f9b591ae70e11132db
Dmitri Djourov <Dmitri.Djourov@gmail.com>
2023-07-27 14:48:22 +0300

    fixed bugs

--- src/main/java/org/example/Filler.java
+++ src/main/java/org/example/Filler.java
@@ -1,16 +1,7 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.Random;
-
 public class Filler {
-
-
-    //Метод для создания и заполнения массива случайными числами
-    public static void createArray(int[] array) {
-//        int[] array = new int[N];
-        Random random = new Random();
-
+    public static void fillArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 10) + 1;
         }
