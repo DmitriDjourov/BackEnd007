05a003bbf5e25780597604f65dc1cada0502ae73
Dmitri Djourov <Dmitri.Djourov@gmail.com>
2023-07-27 16:07:22 +0300

    added new dicha

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         int[] arr = Creator.createArray(5);
-        Filler.fillArray(arr);
+        Filler.fillArray(arr, 20, 10);
 
         Printer.printArray(arr);
     }
