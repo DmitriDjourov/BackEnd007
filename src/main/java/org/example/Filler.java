f5420c720e72aafb20b0b7748345f8f54a2f3cc9
Dmitri Djourov <Dmitri.Djourov@gmail.com>
2023-07-27 14:30:48 +0300

    chanched class

--- src/main/java/org/example/Filler.java
+++ src/main/java/org/example/Filler.java
@@ -4,22 +4,15 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Filler {
-    public static void main(String[] args) {
-        int N = 5;
-        int[] array = createArray(N);
-        System.out.printf("В массиве %s  ", Arrays.toString(array));
 
-
-    }
 
     //Метод для создания и заполнения массива случайными числами
-    public static int[] createArray(int N) {
-        int[] array = new int[N];
+    public static void createArray(int[] array) {
+//        int[] array = new int[N];
         Random random = new Random();
 
-        for (int i = 0; i < N; i++) {
-            array[i] = (int) (Math.random() * N) + 1;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10) + 1;
         }
-        return array;
     }
 }
